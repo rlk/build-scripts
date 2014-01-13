04044dcf3b39fdbd4aff9d025ce0166e76769d97
Robert Kooima <robert.kooima@gmail.com>
2014-01-13 13:52:18 -0600

    Linux compatibility fixes
    Replace /bin/sh with /bin/bash everywhere.
    Protect against INSTALL_PREFIX == PREFIX in INSTALL.sh

--- CONFIG.sh
+++ CONFIG.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+# INSTALL_PREFIX gives the destination to which INSTALL.sh will copy staged
+# package contents.
+
+if [[ $IS_MINGW ]]; then
+    export INSTALL_PREFIX=C:/MinGW
+else
+    export INSTALL_PREFIX=/usr/local
+fi
 
 # PREFIX gives the location passed to a build system for installation, usually
-# `configure --prefix`. It may give a global location like /usr/local, but will
-# commonly give a local staging directory.
+# `configure --prefix`.
 
-export PREFIX=$(PWD)/dst
+#export PREFIX=$INSTALL_PREFIX
+export PREFIX=$PWD/dst
 
 # Let make take advantage of multiple processor cores. Some versions of MinGW
 # seem to deadlock instead.
@@ -27,11 +36,3 @@ if [ $(uname -s) == "MINGW32_NT-6.1" ]; then
     export IS_MINGW=1
 fi
 
-# INSTALL_PREFIX gives the destination to which INSTALL.sh will copy staged
-# package contents.
-
-if [[ $IS_MINGW ]]; then
-    export INSTALL_PREFIX=C:/MinGW
-else
-    export INSTALL_PREFIX=/usr/local
-fi
--- INSTALL.sh
+++ INSTALL.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 source CONFIG.sh
 
-if [ -d "$PREFIX/bin" ]; then
-	cp -R $PREFIX/bin/* $INSTALL_PREFIX/bin
-fi
-if [ -d "$PREFIX/lib" ]; then
-	cp -R $PREFIX/lib/* $INSTALL_PREFIX/lib
-fi
-if [ -d "$PREFIX/share" ]; then
-	cp -R $PREFIX/share/* $INSTALL_PREFIX/share
-fi
-if [ -d "$PREFIX/include" ]; then
-	cp -R $PREFIX/include/* $INSTALL_PREFIX/include
+if [ "$PREFIX" -ne "$INSTALL_PREFIX" ]; then
+    if [ -d "$PREFIX/bin" ]; then
+        cp -R $PREFIX/bin/* $INSTALL_PREFIX/bin
+    fi
+    if [ -d "$PREFIX/lib" ]; then
+        cp -R $PREFIX/lib/* $INSTALL_PREFIX/lib
+    fi
+    if [ -d "$PREFIX/share" ]; then
+        cp -R $PREFIX/share/* $INSTALL_PREFIX/share
+    fi
+    if [ -d "$PREFIX/include" ]; then
+        cp -R $PREFIX/include/* $INSTALL_PREFIX/include
+    fi
 fi
