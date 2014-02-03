22a4dcc5aa5d6fb7f48288607bb1a815eb349a8f
Robert Kooima <robert.kooima@gmail.com>
2014-02-03 12:31:51 -0600

    Add libccd to ODE build

--- build-ode.sh
+++ build-ode.sh
@@ -7,11 +7,12 @@ if [ -n "$1" ]; then
 fi
 
 # ODE has always been infatuated with its own renderer and demonstrations.
-# I don't need these. I do need double precision though.
+# I don't need these. I do need double precision and the libccd colliders.
 
 ./configure --with-drawstuff=None \
             --disable-demos \
             --enable-double-precision \
+            --enable-libccd \
             --prefix=$PREFIX \
     && make \
     && make install
