0cffdd894abba1b164b39b284f8a8ddb01602da9
Robert Kooima <robert.kooima@gmail.com>
2014-05-29 13:26:07 -0500

    Need JPEG support in libtiff recently

--- build-tiff.sh
+++ build-tiff.sh
@@ -6,10 +6,6 @@ if [ -n "$1" ]; then
     cd $1
 fi
 
-# LibTIFF's JPEG support generates conflicts over TRUE and FALSE. Most users
-# would expect to store JPEG data in a JPEG file, so we can skip that feature.
-
-./configure --disable-jpeg \
-            --prefix=$PREFIX \
+./configure --prefix=$PREFIX \
     && make \
     && make install
